02582f88ca6d7d3ed9d51cd587d97cf815a22d2e
RudraPatel5435 <rudranpatel0980@gmail.com>
2025-01-05 20:38:27 +0530

    Sidebar hides on smaller screens

--- client/src/components/HomePage/Sidebar.jsx
+++ client/src/components/HomePage/Sidebar.jsx
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router';
 
 const Sidebar = ({expanded, setExpanded}) => {
   return (
-    <div className='h-100vh w-full px-10 -mt-5'>
+    <div className='hidden md:block h-100vh w-full px-10 -mt-5'>
       <div className={`p-5 h-full w-[300px] ease-out duration-500 bg-slate-700 rounded-xl ${expanded ? "opacity-100" : " opacity-0 translate-x-[-100%]"}`}>
         <div className="text-blue-200 flex justify-between items-center "><div onClick={()=>{setExpanded(!expanded)}} className=' hover:bg-sky-600 text-3xl p-1 rounded-full cursor-pointer'><RxCross2 /></div><div className='text-3xl text-center -translate-x-1/2'>Everyting</div></div>
         <div className="w-full h-1 rounded-full my-3 bg-slate-300"></div>
